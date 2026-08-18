9863ea8e82e45da7b327bc16c40bd0252f5c5f0f
tkarthikeyan2026 <tkarthikeyan2026@gmail.com>
2026-08-18 16:21:13 +0530

    Update home route message for demo branch

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! CI/CD deployment  v2 is working."
+    return "Hello! CI/CD deployment  v2 is working and this is the demo branch."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
